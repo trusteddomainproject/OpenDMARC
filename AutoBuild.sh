466c0ce027c6c97a9d4e1c3f903bc9ac299ee2a7
Bryan Costales <bcx+dmarc@bcx.com>
2012-03-20 11:52:53 -0700

    Added just enough to get the file to build.

---
+++ AutoBuild.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+perform()
+{
+	$*
+	status=$?
+	if [ $status != 0 ]
+	then
+		echo Fatal ERROR: "\"$*\" failed."
+		exit 1
+	fi
+}
+
+case $1 in
+clean)
+	echo "Cleaning"
+	if [ -f Makefile ]
+	then
+		perform "make -s clean"
+	fi
+	rm -f *.gz
+	exit 0
+	;;
+dist)
+	# Put extras here.
+	if [ -f Makefile ]
+	then
+		make dist
+	fi
+	exit 0
+	;;
+*)
+	;;
+esac
+
+echo "Building opendmarc"
+
+if [ ! -f config.h.in -a -f config.h.in~ ]; then
+    perform "cp config.h.in~ config.h.in"
+fi
+
+ACLOCAL=aclocal
+AUTOMAKE=automake
+AUTOHEADER=autoheader
+AUTOCONF=autoconf
+
+for name in $ACLOCAL $AUTOHEADER $AUTOMAKE $AUTOCONF
+do
+	echo $name
+	perform $name
+done
+
+CONF="./configure --with-wall  -q"
+echo ${CONF}
+perform "$CONF"
+perform "make -s"
+exit 0
