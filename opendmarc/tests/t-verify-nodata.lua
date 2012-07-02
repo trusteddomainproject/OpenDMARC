889bfb89d70e1779523c00aff310ad24e5134d8e
Murray S. Kucherawy <msk@trusteddomain.org>
2012-07-01 22:36:40 -0700

    Add first filter unit test

---
+++ opendmarc/tests/t-verify-nodata.lua
@@ -0,0 +1,121 @@
+-- Copyright (c) 2012, The Trusted Domain Project.  All rights reserved.
+
+-- Test message from paypal.com with no DKIM or SPF DATA
+-- 
+-- Confirms that a message with no authentication data claiming to be from a
+-- "p=reject" domain will provoke a rejection.
+
+mt.echo("*** no data reject test")
+
+-- setup
+sock = "unix:" .. mt.getcwd() .. "/t-verify-ss.sock"
+binpath = mt.getcwd() .. "/.."
+if os.getenv("srcdir") ~= nil then
+	mt.chdir(os.getenv("srcdir"))
+end
+
+-- try to start the filter
+mt.startfilter(binpath .. "/opendmarc", "-c", "t-verify-nodata.conf",
+               "-p", sock)
+
+-- try to connect to it
+conn = mt.connect(sock, 40, 0.05)
+if conn == nil then
+	error("mt.connect() failed")
+end
+
+-- send connection information
+-- mt.negotiate() is called implicitly
+if mt.conninfo(conn, "localhost", "127.0.0.1") ~= nil then
+	error("mt.conninfo() failed")
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error("mt.conninfo() unexpected reply")
+end
+
+-- send envelope macros and sender data
+-- mt.helo() is called implicitly
+mt.macro(conn, SMFIC_MAIL, "i", "t-verify-nodata")
+if mt.mailfrom(conn, "user@paypal.com") ~= nil then
+	error("mt.mailfrom() failed")
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error("mt.mailfrom() unexpected reply")
+end
+
+-- send headers
+-- mt.rcptto() is called implicitly
+if mt.header(conn, "From", "user@paypal.com") ~= nil then
+	error("mt.header(From) failed")
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error("mt.header(From) unexpected reply")
+end
+if mt.header(conn, "To", "user@example.com") ~= nil then
+	error("mt.header(To) failed")
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error("mt.header(To) unexpected reply")
+end
+if mt.header(conn, "Date", "Tue, 22 Dec 2009 13:04:12 -0800") ~= nil then
+	error("mt.header(Date) failed")
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error("mt.header(Date) unexpected reply")
+end
+if mt.header(conn, "Subject", "DMARC test") ~= nil then
+	error("mt.header(Subject) failed")
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error("mt.header(Subject) unexpected reply")
+end
+
+-- send EOH
+if mt.eoh(conn) ~= nil then
+	error("mt.eoh() failed")
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error("mt.eoh() unexpected reply")
+end
+
+-- send body
+if mt.bodystring(conn, "This is a test!\r\n") ~= nil then
+	error("mt.bodystring() failed")
+end
+if mt.getreply(conn) ~= SMFIR_CONTINUE then
+	error("mt.bodystring() unexpected reply")
+end
+
+-- end of message; let the filter react
+if mt.eom(conn) ~= nil then
+	error("mt.eom() failed")
+end
+if mt.getreply(conn) ~= SMFIR_ACCEPT then
+	error("mt.eom() unexpected reply")
+end
+
+-- verify that an Authentication-Results header field got added
+if not mt.eom_check(conn, MT_HDRINSERT, "Authentication-Results") and
+   not mt.eom_check(conn, MT_HDRADD, "Authentication-Results") then
+	error("no Authentication-Results added")
+end
+
+-- verify that a DMARC fail result was added
+n = 0
+found = 0
+while true do
+	ar = mt.getheader(conn, "Authentication-Results", n)
+	if ar == nil then
+		break
+	end
+	if string.find(ar, "dmarc=fail", 1, true) ~= nil then
+		found = 1
+		break
+	end
+	n = n + 1
+end
+if found == 0 then
+	error("incorrect DMARC result")
+end
+
+mt.disconnect(conn)
