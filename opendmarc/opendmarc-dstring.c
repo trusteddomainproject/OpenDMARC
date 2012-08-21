113636d84d7e0010fad9925d2a983a2433520bfa
Murray S. Kucherawy <msk@trusteddomain.org>
2012-08-21 00:58:48 -0700

    Fix bounds checking in dmarcf_dstring_printf().  Problem noted by Marcos Digirati.

--- opendmarc/opendmarc-dstring.c
+++ opendmarc/opendmarc-dstring.c
@@ -433,6 +433,7 @@ size_t
 dmarcf_dstring_printf(struct dmarcf_dstring *dstr, char *fmt, ...)
 {
 	size_t len;
+	size_t rem;
 	va_list ap;
 	va_list ap2;
 
@@ -441,20 +442,21 @@ dmarcf_dstring_printf(struct dmarcf_dstring *dstr, char *fmt, ...)
 
 	va_start(ap, fmt);
 	va_copy(ap2, ap);
-	len = vsnprintf((char *) dstr->ds_buf + dstr->ds_len, dstr->ds_alloc,
-	                fmt, ap);
+	rem = dstr->ds_alloc - dstr->ds_len;
+	len = vsnprintf((char *) dstr->ds_buf + dstr->ds_len, rem, fmt, ap);
 	va_end(ap);
 
-	if (len > dstr->ds_len)
+	if (len > rem)
 	{
-		if (!dmarcf_dstring_resize(dstr, len + 1))
+		if (!dmarcf_dstring_resize(dstr, dstr->ds_len + len + 1))
 		{
 			va_end(ap2);
 			return (size_t) -1;
 		}
 
-		len = vsnprintf((char *) dstr->ds_buf + dstr->ds_len,
-		                dstr->ds_alloc, fmt, ap2);
+		rem = dstr->ds_alloc - dstr->ds_len;
+		len = vsnprintf((char *) dstr->ds_buf + dstr->ds_len, rem,
+		                fmt, ap2);
 	}
 
 	va_end(ap2);
