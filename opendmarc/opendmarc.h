59cb2dcb86a89afd15b8f8d1ad8c815df9424e18
Murray S. Kucherawy <msk@trusteddomain.org>
2015-02-10 17:11:53 -0800

    Update copyright

--- opendmarc/opendmarc.h
+++ opendmarc/opendmarc.h
@@ -1,5 +1,6 @@
 /*
-**  Copyright (c) 2012, 2013, The Trusted Domain Project.  All rights reserved.
+**  Copyright (c) 2012, 2013, 2015, The Trusted Domain Project.
+**  	All rights reserved.
 */
 
 #ifndef _OPENDMARC_H_
