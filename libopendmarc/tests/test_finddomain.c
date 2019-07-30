f6b615e345037408b88b2ffd1acd03239af8a858
Marcin Seremak <panpilkarz@protonmail.com>
2019-07-30 08:05:28 +0200

    Fix multiple addresses in From vulnerability

--- libopendmarc/tests/test_finddomain.c
+++ libopendmarc/tests/test_finddomain.c
@@ -23,6 +23,7 @@ main(int argc, char **argv)
 		/* 11 */ {"(,) joe@joe.com", "joe.com"},
 		/* 12 */ {"\"( bob@bob.com)\" joe@joe.com", "joe.com"},
 		/* 12 */ {"From: Davide D'Marco <user@blah.com>", "blah.com"},
+		/* 13 */ {"blah.com", "blah.com"},
 			 {NULL, NULL},
 	};
 	u_char dbuf[256];
