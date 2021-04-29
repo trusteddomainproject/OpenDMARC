eaeb4c3f60e183ce69c44fba62039b8e974e9115
Murray S. Kucherawy <msk@trusteddomain.org>
2021-04-29 12:28:14 -0700

    Revert "Fix absurd test in opendmarc_spf_dns_lookup_a_actual()."
    This reverts commit 24e969941dec6d7944d34330a0657ce97eb57795.

--- libopendmarc/opendmarc_spf_dns.c
+++ libopendmarc/opendmarc_spf_dns.c
@@ -100,7 +100,7 @@ opendmarc_spf_dns_lookup_a_actual(char *domain, int sought, char **ary, int *cnt
 		*++bp = '.';
 	*++bp = '\0';
 	/* 
-	 * Make sure host does not begin with a dot.
+	 * Make user host does not begin with a dot.
 	 */
 	bp = hbuf;
 	if (*bp == '.')
@@ -165,7 +165,6 @@ opendmarc_spf_dns_lookup_a_actual(char *domain, int sought, char **ary, int *cnt
 		GETSHORT(l, cp);
 		if (type == T_CNAME)
 		{
-			/* CNAME; walk past the expanded name and continue */
 			char cname[MAXDNSHOSTNAME + 1];
 
 			k = dn_expand((u_char *) &a_buf, eom, cp,
@@ -173,9 +172,8 @@ opendmarc_spf_dns_lookup_a_actual(char *domain, int sought, char **ary, int *cnt
 			cp += k;
 			continue;
 		}
-		else if (type != T_TXT)
+		if (type != type)
 		{
-			/* not a TXT; walk past the whole thing and continue */
 			cp += l;
 			continue;
 		}
