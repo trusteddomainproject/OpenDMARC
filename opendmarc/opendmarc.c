a28ec51298e134f024dcc4da09c0f313b2485bac
Dan Mahoney <github@gushiorg>
2026-05-17 00:41:46 -0700

    Replace AUTHRESHDRNAME with AUTHRESULTSHDR, remove duplicate macro (issue #20)
    AUTHRESHDRNAME in opendmarc-ar.h and AUTHRESULTSHDR in opendmarc.h both
    defined the same string. Remove the duplicate and use AUTHRESULTSHDR
    consistently throughout.

--- opendmarc/opendmarc.c
+++ opendmarc/opendmarc.c
@@ -2496,7 +2496,7 @@ mlfi_eom(SMFICTX *ctx)
 	     hdr = hdr->hdr_next, c++)
 	{
 		/* skip it if it's not Authentication-Results */
-		if (strcasecmp(hdr->hdr_name, AUTHRESHDRNAME) != 0)
+		if (strcasecmp(hdr->hdr_name, AUTHRESULTSHDR) != 0)
 			continue;
 
 		/* parse it */
