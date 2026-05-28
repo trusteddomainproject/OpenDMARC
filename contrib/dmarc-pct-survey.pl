5a1ec2d4c94b6269740178d787d951bdad37b54a
Dan Mahoney <github@gushi.org>
2026-05-28 08:43:36 -0700

    contrib: add dmarc-pct-survey.pl

---
+++ contrib/dmarc-pct-survey.pl
@@ -0,0 +1,150 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Net::DNS;
+use IO::Select;
+use Getopt::Long;
+
+# Query Umbrella top-1M domains for DMARC records, reporting pct= usage.
+#
+# Output (STDOUT): TSV - domain, pct_value, full_record
+# Progress/stats (STDERR): running count + final summary
+
+my $concurrency = 500;
+my $timeout     = 5;
+my $infile      = 'top-1m.csv';
+my $max_domains = 0;  # 0 = unlimited
+my $nameserver;       # undef = system default
+
+GetOptions(
+    'concurrency=i' => \$concurrency,
+    'timeout=i'     => \$timeout,
+    'input=s'       => \$infile,
+    'max=i'         => \$max_domains,
+    'nameserver=s'  => \$nameserver,
+) or die "Usage: $0 [--input FILE] [--concurrency N] [--timeout N] [--max N] [--nameserver IP]\n";
+
+my $resolver = Net::DNS::Resolver->new(
+    udp_timeout => $timeout,
+    tcp_timeout => $timeout,
+    retrans     => 1,
+    retry       => 1,
+    ($nameserver ? (nameservers => [$nameserver]) : ()),
+);
+
+open(my $fh, '<', $infile) or die "Cannot open $infile: $!\n";
+
+# Stats
+my $n_queued   = 0;
+my $n_done     = 0;
+my $n_dmarc    = 0;  # has v=DMARC1 record
+my $n_pct      = 0;  # has pct= in record
+my $n_errors   = 0;
+
+# In-flight: socket => domain
+my %inflight;
+my $sel = IO::Select->new;
+
+my $progress_interval = 10_000;
+my $next_progress     = $progress_interval;
+
+sub dispatch {
+    my ($domain) = @_;
+    my $qname  = "_dmarc.$domain";
+    my $socket = $resolver->bgsend($qname, 'TXT');
+    unless ($socket) {
+        $n_errors++;
+        $n_done++;
+        return;
+    }
+    $inflight{$socket} = $domain;
+    $sel->add($socket);
+    $n_queued++;
+}
+
+sub harvest {
+    my ($block) = @_;
+    my @ready = $block ? $sel->can_read($timeout) : $sel->can_read(0);
+    for my $sock (@ready) {
+        my $domain = delete $inflight{$sock};
+        $sel->remove($sock);
+        $n_done++;
+
+        my $pkt = eval { $resolver->bgread($sock) };
+        unless ($pkt) {
+            $n_errors++;
+            next;
+        }
+
+        my $rcode = $pkt->header->rcode;
+        # NXDOMAIN and NOERROR-with-no-answers are normal (domain has no record)
+        next if $rcode eq 'NXDOMAIN';
+        next if $rcode eq 'NOERROR' && !($pkt->answer);
+
+        for my $rr ($pkt->answer) {
+            next unless $rr->type eq 'TXT';
+            my $txt = join('', $rr->txtdata);
+            next unless $txt =~ /^v=DMARC1\b/i;
+
+            $n_dmarc++;
+
+            if ($txt =~ /\bpct=(\d+)/i) {
+                my $pct = $1;
+                $n_pct++;
+                print join("\t", $domain, $pct, $txt), "\n";
+            }
+            last;  # only evaluate first v=DMARC1 record
+        }
+    }
+}
+
+# Print TSV header to STDOUT
+print join("\t", "domain", "pct", "record"), "\n";
+
+print STDERR "Reading $infile, concurrency=$concurrency, timeout=${timeout}s\n";
+
+while (my $line = <$fh>) {
+    chomp $line;
+    next unless $line =~ /\S/;
+
+    # Handle both "rank,domain" and plain "domain"
+    my $domain = ($line =~ /^\d+,(.+)$/) ? $1 : $line;
+    $domain =~ s/^\s+|\s+$//g;
+    next unless $domain =~ /\./;
+
+    dispatch($domain);
+
+    # Drain when we've filled the concurrency window
+    while (scalar(keys %inflight) >= $concurrency) {
+        harvest(1);
+    }
+
+    # Opportunistic non-blocking harvest
+    harvest(0);
+
+    if ($n_done >= $next_progress) {
+        printf STDERR "  %d done, %d in-flight, %d dmarc, %d pct\n",
+            $n_done, scalar(keys %inflight), $n_dmarc, $n_pct;
+        $next_progress += $progress_interval;
+    }
+
+    last if $max_domains && $n_queued >= $max_domains;
+}
+
+close($fh);
+
+# Drain remaining in-flight queries
+while (%inflight) {
+    harvest(1);
+    if ($n_done >= $next_progress) {
+        printf STDERR "  %d done, %d in-flight, %d dmarc, %d pct\n",
+            $n_done, scalar(keys %inflight), $n_dmarc, $n_pct;
+        $next_progress += $progress_interval;
+    }
+}
+
+printf STDERR "\nDone.\n";
+printf STDERR "  Domains queried : %d\n", $n_done;
+printf STDERR "  Errors          : %d\n", $n_errors;
+printf STDERR "  Have DMARC      : %d (%.1f%%)\n", $n_dmarc, $n_done ? 100*$n_dmarc/$n_done : 0;
+printf STDERR "  Have pct=       : %d (%.1f%% of DMARC)\n", $n_pct, $n_dmarc ? 100*$n_pct/$n_dmarc : 0;
