09ef2aa25db9ae959b2f39541d417c49bcf09312
Dan Mahoney <github@gushiorg>
2026-06-15 00:19:07 -0400

    feat: count no-op pct= usage in dmarc-pct-survey.pl
    Tracks two common misconfigurations that have no effect on policy:
    - p=reject/quarantine with pct=100 (pct=100 is the default, a no-op)
    - p=none with any pct= value (pct only applies to reject/quarantine)

--- contrib/dmarc-pct-survey.pl
+++ contrib/dmarc-pct-survey.pl
@@ -78,6 +78,8 @@ my $n_psd_n    = 0;  # has psd=n
 my $n_t        = 0;  # has t= in record
 my $n_t_y      = 0;  # has t=y
 my $n_t_n      = 0;  # has t=n
+my $n_pct100_noop = 0;  # p=reject or p=quarantine with pct=100 (a no-op)
+my $n_none_pct    = 0;  # p=none with any pct= value (has no effect)
 my $n_errors   = 0;
 
 # In-flight: socket => [ domain, dispatch_time ]
@@ -131,6 +133,7 @@ sub harvest {
             my $pct = ($txt =~ /\bpct=(\d+)/i)  ? $1      : '-';
             my $psd = ($txt =~ /\bpsd=([yn])/i)  ? lc($1) : '-';
             my $t   = ($txt =~ /\bt=([yn])/i)    ? lc($1) : '-';
+            my $p   = ($txt =~ /\bp=(\w+)/i)     ? lc($1) : '-';
 
             $n_pct++ if $pct ne '-';
             if ($psd ne '-') {
@@ -142,6 +145,13 @@ sub harvest {
                 $n_t++;
                 $n_t_y++ if $t eq 'y';
                 $n_t_n++ if $t eq 'n';
+            }
+
+            if (($p eq 'reject' || $p eq 'quarantine') && $pct eq '100') {
+                $n_pct100_noop++;
+            }
+            if ($p eq 'none' && $pct ne '-') {
+                $n_none_pct++;
             }
 
             print $out join("\t", $run_date, $domain, $pct, $psd, $t, $txt), "\n";
@@ -221,9 +231,11 @@ printf STDERR "    psd=n         : %d\n", $n_psd_n;
 printf STDERR "  Have t=         : %d (%.1f%% of DMARC)\n", $n_t,     $n_dmarc ? 100*$n_t/$n_dmarc     : 0;
 printf STDERR "    t=y           : %d\n", $n_t_y;
 printf STDERR "    t=n           : %d\n", $n_t_n;
+printf STDERR "  p=reject/quarantine with pct=100 (no-op) : %d\n", $n_pct100_noop;
+printf STDERR "  p=none with pct= (no effect)             : %d\n", $n_none_pct;
 
 my $is_new = !-f $summarylog;
 open(my $sum, '>>', $summarylog) or die "Cannot open $summarylog: $!\n";
-print $sum join("\t", qw(run_date queried errors have_dmarc pct_total psd_total psd_y psd_n t_total t_y t_n)), "\n" if $is_new;
-print $sum join("\t", $run_date, $n_done, $n_errors, $n_dmarc, $n_pct, $n_psd, $n_psd_y, $n_psd_n, $n_t, $n_t_y, $n_t_n), "\n";
+print $sum join("\t", qw(run_date queried errors have_dmarc pct_total psd_total psd_y psd_n t_total t_y t_n pct100_noop none_pct)), "\n" if $is_new;
+print $sum join("\t", $run_date, $n_done, $n_errors, $n_dmarc, $n_pct, $n_psd, $n_psd_y, $n_psd_n, $n_t, $n_t_y, $n_t_n, $n_pct100_noop, $n_none_pct), "\n";
 close($sum);
